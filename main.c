5627ec6ab461204db9fad6872ecc20c298903b3f
Dennis <dennisconcepcionmartin@gmail.com>
2023-06-12 18:24:19 +0100

    draw a point

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <GL/glut.h>
+
+
+#define HEIGHT 1080
+#define WIDTH 1920
+
+
+void setup() {
+	
+	// Set background dark
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	// Set point color and size to 1 pixel
+	glColor3f(0.0, 1.0, 0.0);
+	glPointSize(5.0);
+
+	// Matrix projection and reset with identity
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	// Set the coordinates to be used with the viewport
+	gluOrtho2D(0, WIDTH, HEIGHT, 0);
+}
+
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POINTS);
+	glVertex2i(1920/2, 1080/2);
+	glEnd();
+	glFlush();
+}
+
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("OpenGL Window");
+	setup();
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
