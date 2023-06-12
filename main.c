f315722623a73b338e858e4b1e46500efb9d6902
Dennis <dennisconcepcionmartin@gmail.com>
2023-06-12 18:45:30 +0100

    draw a weird filled rectangle

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 
 void setup() {
-	
+
 	// Set background dark
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 
@@ -18,17 +18,20 @@ void setup() {
 	// Matrix projection and reset with identity
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	
+
 	// Set the coordinates to be used with the viewport
-	gluOrtho2D(0, WIDTH, HEIGHT, 0);
+	gluOrtho2D(-300, 300, 300, -300);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
 }
 
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POINTS);
-	glVertex2i(1920/2, 1080/2);
-	glEnd();
+	glRectf(100.0f, 100.0f, 200.0f, 200.0f);
+	//	glVertex2i(1920/2, 1080/2);
 	glFlush();
 }
 
