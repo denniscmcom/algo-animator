5c3e4d7420f70283b8e46bb2b39bef2822010302
Dennis <dennisconcepcionmartin@gmail.com>
2023-06-23 10:01:37 +0100

    refactor

---
+++ algos.h
@@ -0,0 +1,5 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+
+
+#endif
--- utils.c
+++ utils.c
@@ -1,23 +1,6 @@
 #include "utils.h"
 
 
-void render_text(const char *text, float x, float y) {
-	glRasterPos2f(x, y);
-
-	for (const char *c = text; *c; ++c) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c);
-	}
-}
-
-
 int random_int(int min, int max) {
 	return rand() % ((max - min) + 1) + min;
-}
-
-
-void draw_rectangle(int x_position, int rect_height, int rect_width, int window_height) {
-	glVertex2f(x_position, window_height - 100);
-	glVertex2f(x_position + rect_width, window_height - 100);
-	glVertex2f(x_position + rect_width, rect_height);
-	glVertex2d(x_position, rect_height);
 }
